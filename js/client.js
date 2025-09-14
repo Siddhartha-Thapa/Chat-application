8bd24eab4b1c40bb45ffb043a5573a9df1a383fb
Siddhartha Thapa <siddharthathapa001@gmail.com>
2025-09-14 23:52:04 +0545

    changes

--- js/client.js
+++ js/client.js
@@ -2,8 +2,18 @@ const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
 const messageinput = document.getElementById('messageInp');
-const messagecontainer = document.querySelector('.container');
+const messagecontainer = document.querySelector(".container");
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messagecontainer.append(messageElement);
+}
 const uname = prompt("Enter your name to join the chat");
-socket.on('connect', () => {
-    socket.emit('new-user', uname);
-});
+socket.emit('new-user', uname);
+
+socket.on('user-joined', uname=>{
+    append(`${uname} joined the chat`, 'right');
+
+})
