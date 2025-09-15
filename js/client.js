e63d0b4e8185e943d7c807bba6dae63ff92b835c
Siddhartha Thapa <siddharthathapa001@gmail.com>
2025-09-15 08:52:18 +0545

    changes

--- js/client.js
+++ js/client.js
@@ -3,17 +3,39 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageinput = document.getElementById('messageInp');
 const messagecontainer = document.querySelector(".container");
+var audio = new Audio('ring.mp3');
 const append = (message, position)=>{
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messagecontainer.append(messageElement);
+    if(position==='left'){
+        audio.play();
+    }
 }
 const uname = prompt("Enter your name to join the chat");
 socket.emit('new-user', uname);
 
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageinput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send' , message);
+    messageinput.value = '';
+
+} )
+
 socket.on('user-joined', uname=>{
     append(`${uname} joined the chat`, 'right');
 
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left');
+} )
+
+socket.on('user-left', name=>{
+    append(`${name} left the chat`,'left');
 })
--- nodeserver/index.js
+++ nodeserver/index.js
@@ -12,7 +12,6 @@ const users = {};
 
 io.on('connection',socket=>{
     socket.on('new-user', name=>{
-        console.log("New user", name);
         users[socket.id]= name;
         socket.broadcast.emit('user-joined',name);
     });
@@ -21,4 +20,8 @@ io.on('connection',socket=>{
         socket.broadcast.emit('receive', {message:message , name:users[socket.id]});
 
     });
+    socket.on('disconnect', ()=>{
+    socket.broadcast.emit('user-left', users[socket.id]);
+})
+
 })
